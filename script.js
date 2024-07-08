d24a4d80a424c02183b9ebd3f11d38b24abfecf7
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2024-07-08 23:32:23 +0200

    commit

--- script.js
+++ script.js
@@ -1,68 +1,30 @@
-// var typed = new Typed("#heading-primary", {
-//   strings: ["Nijaz Andelić"],
-//   typeSpeed: 50,
-// });
-// var typed = new Typed("#subheading", {
-//   strings: [
-//     "SOFTWARE ENGINEER & FRONTEND ^1000 DEVELOPER ^1000",
-//     "REACT",
-//     "REACT NATIVE",
-//     "JAVASCRIPT",
-//     "TYPESCRIPT",
-//     "MYSQL",
-//     "PHP",
-//   ],
-//   typeSpeed: 50,
-// });
-// var typed = new Typed("#element", {
-//   strings: ["<i>Nijaz</i> Andelić.", "&amp; a second sentence."],
-//   typeSpeed: 50,
-// });
-
-// var typed = new Typed("#typed", {
-//   stringsElement: "#typed-strings",
-//   typeSpeed: 0,
-//   backSpeed: 0,
-//   backDelay: 500,
-//   startDelay: 1000,
-//   loop: false,
-//   onBegin: function (self) {
-//     prettyLog("onBegin " + self);
-//   },
-//   onComplete: function (self) {
-//     prettyLog("onCmplete " + self);
-//   },
-//   preStringTyped: function (pos, self) {
-//     prettyLog("preStringTyped " + pos + " " + self);
-//   },
-//   onStringTyped: function (pos, self) {
-//     prettyLog("onStringTyped " + pos + " " + self);
-//   },
-//   onLastStringBackspaced: function (self) {
-//     prettyLog("onLastStringBackspaced " + self);
-//   },
-//   onTypingPaused: function (pos, self) {
-//     prettyLog("onTypingPaused " + pos + " " + self);
-//   },
-//   onTypingResumed: function (pos, self) {
-//     prettyLog("onTypingResumed " + pos + " " + self);
-//   },
-//   onReset: function (self) {
-//     prettyLog("onReset " + self);
-//   },
-//   onStop: function (pos, self) {
-//     prettyLog("onStop " + pos + " " + self);
-//   },
-//   onStart: function (pos, self) {
-//     prettyLog("onStart " + pos + " " + self);
-//   },
-//   onDestroy: function (self) {
-//     prettyLog("onDestroy " + self);
-//   },
-// });
-
 var typed = new Typed("#typed", {
   stringsElement: "#typed-strings",
   loop: true,
   typeSpeed: 50,
+});
+
+const footerDate = document.getElementById("date");
+if (footerDate) footerDate.textContent = new Date().getFullYear();
+
+function toggleText(id) {
+  const testimonial = document.getElementById(id);
+  const text = testimonial.querySelector(".text");
+  const moreText = text.querySelector(".more-text");
+  const btn = testimonial.querySelector(".read-more-btn");
+
+  if (moreText.style.display === "none" || moreText.style.display === "") {
+    moreText.style.display = "inline";
+    text.style.maxHeight = "none";
+    btn.textContent = "Read Less";
+  } else {
+    moreText.style.display = "none";
+    text.style.maxHeight = "150px";
+    btn.textContent = "Read More";
+  }
+}
+
+document.querySelectorAll(".testimonial-content").forEach((testimonial) => {
+  const moreText = testimonial.querySelector(".more-text");
+  if (moreText) moreText.style.display = "none";
 });
